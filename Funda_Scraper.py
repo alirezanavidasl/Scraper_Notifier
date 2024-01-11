da35ebcb23ccf14758a830dbc6453562d337e088
alirezanavidasl <alireza@passivelogic.com>
2024-01-11 17:21:33 +0100

    add funda scraper

---
+++ Funda_Scraper.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+from Basecraper import Basecraper
+
+
+class Funda_Scraper(Basecraper):
+
+    
+
+    def scrape(self):
+
+        headers = {
+                        'accept': '*/*',
+                        'accept-language': 'en-US,en;q=0.9,fa;q=0.8',
+                        'origin': 'https://www.funda.nl',
+                        'referer': 'https://www.funda.nl/',
+                        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+                        'sec-ch-ua-mobile': '?0',
+                        'sec-ch-ua-platform': '"macOS"',
+                        'sec-fetch-dest': 'empty',
+                        'sec-fetch-mode': 'cors',
+                        'sec-fetch-site': 'cross-site',
+                        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                    }
+
+        try:
+            response = requests.get(self.url,headers=headers)
+
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'lxml')
+                items = soup.find_all('div', class_='sm:flex')
+
+                for item in items:
+                    item_content={}
+                    property_info = items[0].find('div',class_='min-w-0').text
+
+                    # Split the string into lines and remove empty lines
+                    lines = [line.strip() for line in property_info.split('\n') if line.strip()]
+            
+                    # Extracted information
+                    address = lines[0]
+                    postcode = lines[1]
+                    rent = lines[2]
+                    area = lines[3]
+                    rooms = lines[4]
+                    agent = lines[5]
+
+                    specs = f"""
+                            Address : {address}
+                            PostCode : {postcode}
+                            Rent : {rent}
+                            Area : {area}
+                            Rooms : {rooms}
+                            Agent : {agent}
+
+                            """
+
+                    item_content["specs"] = specs
+                    item_content["item_url"] = item.find('a').get('href')
+
+                    self.check_item(item_content)
+
+                            
+
+        except Exception as e:
+            
+            self.notify_log_error(e)
+
+            
+
