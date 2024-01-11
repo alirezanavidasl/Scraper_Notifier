b1cb4b008ded5591c8a34462f52941d71b82a522
alirezanavidasl <alireza@passivelogic.com>
2024-01-11 17:21:54 +0100

    class for test runs

---
+++ SingleRunner.py
@@ -0,0 +1,29 @@
+from H2S_Scraper import H2S_Scraper
+from Pararius_Scraper import Pararius_Scraper
+from Kamernet_Scraper import Kamernet_Scraper
+from Funda_Scraper import Funda_Scraper
+
+import json
+import random,time
+
+with open('../Data.json', 'r') as file:
+    data_dict = json.load(file)
+
+scraperInstances = []
+
+for classData in data_dict["Sites_data"]:
+        
+        scraperObject = getattr(__import__(__name__), f'{classData["Name"]}_Scraper')
+        scraperInstance = scraperObject(classData)
+        scraperInstances.append(scraperInstance)
+
+while True:
+    for scraperInstance in scraperInstances:
+        scraperInstance.run()
+        scraperInstance.logger.log_cleaner()
+    time.sleep(10 + random.randint(1, 4))
+
+
+
+
+
